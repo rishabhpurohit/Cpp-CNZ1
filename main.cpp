f0434145aecb0d1abe3a803e8bab6a31a5773a8f
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-01-22 16:51:17 +0530

    -up1.5

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include <cmath>
-#include <bits/stdc++.h>
 #include <iostream>
-
-#include "class.cpp"
+#include <cmath>
+#include "node_class.cpp"
 
 using namespace std;
 
@@ -12,21 +10,29 @@ void print(node * head){
         cout<<(*temp).data<<" "; //temp ->data
         temp = (*temp).next; // temp -> next these also work ..........its the same
     }
+    cout<<endl;
+    temp = head;
+    while(temp!=NULL){
+        cout<<(*temp).data<<" "; //temp ->data
+        temp = (*temp).next; // temp -> next these also work ..........its the same
+    }
 }
 
 int main(){
     node n1(1);
     node *head = &n1;
-    node n2(2);
 
+    node n2(2);
     node n3(3);
     node n4(4);
     node n5(5);
+
     n1.next = &n2;
     n2.next = &n3;
     n3.next = &n4;
     n4.next = &n5;
     n5.next = NULL;
+
     print(head);
 }
 
--- print a LL.cpp
+++ print a LL.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 #include <iostream>
 
-#include "class.cpp"
+#include "node_class.cpp"
 
 using namespace std;
 
@@ -17,16 +17,18 @@ void print(node * head){
 int main(){
     node n1(1);
     node *head = &n1;
-    node n2(2);
 
+    node n2(2);
     node n3(3);
     node n4(4);
     node n5(5);
+
     n1.next = &n2;
     n2.next = &n3;
     n3.next = &n4;
     n4.next = &n5;
     n5.next = NULL;
+
     print(head);
 }
 
