676ced1356448cbf7a2b5e3cb888d57707a32d76
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-03-09 19:41:25 +0530

    -up2..4

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include "QueueUsingArray.h"
+
 using namespace std;
 
 int main(){
-    QueueUsingArray<int> q(1);
-    //q.enqueue(0);
-    q.enqueue(1);
-    q.enqueue(2);
-    q.enqueue(3);
-    q.enqueue(4);
-    q.enqueue(5);
-
-
-    cout<<q.front()<<endl;
-    cout<<"removed - "<<q.dequeue()<<endl;
-    cout<<q.front()<<endl;
-    cout<<"removed - "<<q.dequeue()<<endl;
-    cout<<q.front()<<endl;
-    cout<<"removed - "<<q.dequeue()<<endl;
-    cout<<q.front()<<endl;
-    cout<<"removed - "<<q.dequeue()<<endl;
-    cout<<q.front()<<endl;
-    cout<<"removed - "<<q.dequeue()<<endl;
-    cout<<q.front()<<endl;
-    cout<< (!q.isEmpty() ? "Not_EMPTY":"EMPTY") <<endl;
-    cout<<q.getSize()<<endl;
-
+    int n=0,m=0;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    int Xo =0;
+    for(int i=1;i<n;++i){
+        Xo = Xo ^ a[i][1];
+    }
+    if(Xo){
+        cout<<"TAK"<<endl;
+        for(int i=1;i<n;i++){
+            cout<<1<<" ";
+        }
+        return 0;
+    }
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= m; ++j){
+            if(a[i][j] != a[i][1]){
+                cout << "TAK" << '\n';
+                for(int k = 1; k <= n; ++k){
+                    if (k != i) cout << 1 << ' ';
+                    else cout << j << ' ';
+                }
+                return 0;
+            }
+        }
+    }
+    cout << "NIE" << '\n';
     return 0;
 }
